871cf141af7965a93d2d6da6568acc52f5683145
Parsha Pourkhomami <parshap+git@gmail.com>
2013-08-30 13:48:14 -0700

    Add tests for safe filenames

---
+++ test.js
@@ -0,0 +1,21 @@
+var test = require("tape"),
+	sanitize = require("./");
+
+test("valid names", function(t) {
+	["valid name.mp3", "résumé"].forEach(function(name) {
+		t.equal(sanitize(name), name);
+	});
+	t.end();
+});
+
+test("control characters", function(t) {
+	t.equal(sanitize("hello\nworld"), "helloworld");
+	t.end();
+});
+
+test("restricted codes", function(t) {
+	["h?w", "h/w", "h*w"].forEach(function(name) {
+		t.equal(sanitize(name), "hw");
+	});
+	t.end();
+});
