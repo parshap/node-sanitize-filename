9eb3d662ad58d7725792e6dfbfcf06157b1cb683
Thorsten Basse <tbasse@atlassian.com>
2014-07-31 13:39:58 +1000

    Updated test.js

--- test.js
+++ test.js
@@ -8,8 +8,20 @@ test("valid names", function(t) {
 	t.end();
 });
 
+test("valid names", function(t) {
+	["valid name.mp3", "résumé"].forEach(function(name) {
+		t.equal(sanitize(name, "_"), name);
+	});
+	t.end();
+});
+
 test("null character", function(t) {
 	t.equal(sanitize("hello\u0000world"), "helloworld");
+	t.end();
+});
+
+test("null character", function(t) {
+	t.equal(sanitize("hello\u0000world", "_"), "hello_world");
 	t.end();
 });
 
@@ -18,9 +30,21 @@ test("control characters", function(t) {
 	t.end();
 });
 
+test("control characters", function(t) {
+	t.equal(sanitize("hello\nworld", "_"), "hello_world");
+	t.end();
+});
+
 test("restricted codes", function(t) {
 	["h?w", "h/w", "h*w"].forEach(function(name) {
 		t.equal(sanitize(name), "hw");
+	});
+	t.end();
+});
+
+test("restricted codes", function(t) {
+	["h?w", "h/w", "h*w"].forEach(function(name) {
+		t.equal(sanitize(name, "_"), "h_w");
 	});
 	t.end();
 });
