48d1a8b827d9dc93149286f5a873965b90de5c92
Parsha Pourkhomami <parshap+git@gmail.com>
2015-01-26 12:52:00 -0800

    Split regex into multiple regexes

--- index.js
+++ index.js
@@ -13,13 +13,20 @@
  * Unicode Control codes
  * C0 0x00-0x1f & C1 (0x80-0x9f)
  * http://en.wikipedia.org/wiki/C0_and_C1_control_codes
+ *
+ * Reserved filenames (".", "..")
  * 
  * @param  {String} input   Original filename
  * @param  {Object} options {replacement: String}
  * @return {String}         Sanitized filename
  */
+
+var illegalRe = /[\/\?<>\\:\*\|":]/g;
+var controlRe = /[\x00-\x1f\x80-\x9fj]/g;
+
 module.exports = function (input, options) {
-  var replacement  = (options && options.replacement) || '',
-      regex        = /[\/\?<>\\:\*\|":\x00-\x1f\x80-\x9f]/g;
-  return input.replace(regex, replacement);
+  var replacement  = (options && options.replacement) || '';
+  return input
+    .replace(illegalRe, replacement)
+    .replace(controlRe, replacement);
 };
