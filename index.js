6bc8431f54834d3a2914c6343249d1a79fc6b5fe
Joel Mukuthu <joelmukuthu@gmail.com>
2015-03-18 15:38:03 +0100

    Sanitization for reserved filenames in Windows
    Strips out any filenames matching CON, PRN, AUX, NUL, COM1, COM2, COM3, COM4, COM5, COM6, COM7, COM8, COM9, LPT1, LPT2, LPT3, LPT4, LPT5, LPT6, LPT7, LPT8, and LPT9, case-insensitively, with or without an extension.

--- index.js
+++ index.js
@@ -24,11 +24,13 @@
 var illegalRe = /[\/\?<>\\:\*\|":]/g;
 var controlRe = /[\x00-\x1f\x80-\x9f]/g;
 var reservedRe = /^\.+$/;
+var windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])\..*$/i;
 
 module.exports = function (input, options) {
   var replacement  = (options && options.replacement) || '';
   return input
     .replace(illegalRe, replacement)
     .replace(controlRe, replacement)
-    .replace(reservedRe, replacement);
+    .replace(reservedRe, replacement)
+    .replace(windowsReservedRe, replacement);
 };
