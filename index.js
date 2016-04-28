a55b3ba097cafd79717e2b87735ab4885355aac4
Mike Ralphson <mike.ralphson@gmail.com>
2016-04-28 10:20:04 +0100

    Make windows-trailing character tests pass

--- index.js
+++ index.js
@@ -34,13 +34,15 @@ var illegalRe = /[\/\?<>\\:\*\|":]/g;
 var controlRe = /[\x00-\x1f\x80-\x9f]/g;
 var reservedRe = /^\.+$/;
 var windowsReservedRe = /^(con|prn|aux|nul|com[0-9]|lpt[0-9])(\..*)?$/i;
+var windowsTrailingRe = /[\. ]+$/;
 
 function sanitize(input, replacement) {
   var sanitized = input
     .replace(illegalRe, replacement)
     .replace(controlRe, replacement)
     .replace(reservedRe, replacement)
-    .replace(windowsReservedRe, replacement);
+    .replace(windowsReservedRe, replacement)
+    .replace(windowsTrailingRe, replacement);
   return truncate(sanitized, 255);
 }
 
