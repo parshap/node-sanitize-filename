09b71fe996fce7aa086dc9fde30e5e06f5033fe3
Tom Jenkinson <tom@tjenkinson.me>
2019-08-24 15:12:53 +0100

    support function replacement

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
 declare function sanitize(
   input: string,
-  options?: { replacement?: string }
+  options?: {
+    replacement?: string | ((substring: string) => string);
+  }
 ): string;
 
 export = sanitize;
