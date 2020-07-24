2b929ed56f845aa838454cd2cd2b4bdbf243c91e
Nik Malenovic <nik.malenovic@gmail.com>
2020-07-24 10:55:45 -0700

    typescript type declaration for options.truncate

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,7 @@ declare function sanitize(
   input: string,
   options?: {
     replacement?: string | ((substring: string) => string);
+    truncate?: number | ((filename: string) => string);
   }
 ): string;
 
